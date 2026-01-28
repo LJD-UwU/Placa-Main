cb06cc59eb6835e60aee84b998adcf732ea14753
LJD-UwU <himexpe.interns@hisense.com>
2026-01-28 12:48:35 -0800

    Update sap_config.py

--- backend/config/sap_config.py
+++ backend/config/sap_config.py
@@ -1,13 +1,13 @@
 # CONFIGURACIÓN SAP
 
 # Ruta SAP Logon
-SAP_LOGON_PATH = r"C:\Program Files (x86)\SAP\FrontEnd\SAPgui\saplogon.exe"
+SAP_LOGON_PATH = r""
 SAP_SYSTEM_NAME = ""
 
 # Carpeta temporal donde SAP exporta los XLS
-SAP_TMP_PATH = r"C:\Users\admin\AppData\Local\SAP\SAP GUI\tmp"
+SAP_TMP_PATH = r""
 
-EXPORT_FINAL_PATH = r"C:\Users\admin\Documents\Practicante Archivos Main\Automatizacion"
+EXPORT_FINAL_PATH = r""
 
 # ⚠ Credenciales (no recomendado en productivo)
 SAP_USER = ""

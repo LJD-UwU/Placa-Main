cc96961fdb85d4b78dd06c33ead2f7dd228c2c29
LJD-UwU <himexpe.interns@hisense.com>
2026-03-13 13:27:55 -0700

    Ruta

--- backend/config/sap_config.py
+++ backend/config/sap_config.py
@@ -7,7 +7,7 @@ SAP_LOGON_PATH = r"C:\Program Files (x86)\SAP\FrontEnd\SAPgui\saplogon.exe"
 EXPORT_FINAL_PATH = os.path.join(os.path.expanduser("~"), "Documents")
 
 #! Ruta archivos de los submateriales
-EXTRAER_ARCHIVO = r"\\172.29.172.155\Industrial_Eng\IE-SAP\1) BOM files\e) Submaterial Usage"
+EXTRAER_ARCHIVO = r""
 
 #* CONFIGURACIONES MODULOS NO MOVER A MENOS DE QUE SEA CAMBIOS O MODFICACIONES REUQERIDAS
 DESCRIPCIONES = ["主板大组件\\", "主板总成\\", "主板组件\\"]
